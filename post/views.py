8f71d812f7bdf8e853a0ca9e3f8f4461e9cec10f
tkmahesh <hsehamtk2299@gmail.com>
2017-06-14 19:56:29 +0530

    Delete views.py

--- post/views.py
+++
@@ -1,6 +0,0 @@
-from __future__ import unicode_literals
-from django.template import loader
-from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Post,Reply
-# Create your views here.
