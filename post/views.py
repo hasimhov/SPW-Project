184162e7cdde7fcf9d852203895e32275250c2b1
tkmahesh <hsehamtk2299@gmail.com>
2017-06-14 19:55:44 +0530

    Create views.py

--- post/views.py
+++ post/views.py
@@ -4,5 +4,3 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post,Reply
 # Create your views here.
-def wall(request):
-	return render(request,'post/hello.html',{'name': request.session['email']})
