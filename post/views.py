3bd3b5f948b108af39d79b818fc728d0ae559903
T K Mahesh <hsehamtk2299@gmail.com>
2017-06-15 10:45:33 +0530

    Changed post.html

---
+++ post/views.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+from django.template import loader
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .models import Post,Reply,User
+# Create your views here.
+def wall(request):
+	user = User.objects.get(emailId=str(request.session['email']))
+	posts = user.post_set.all()
+	postsrep = []
+	for post in posts:
+		postsrep.append({'post':post,'replies':post.reply_set.all()})
+	context = {
+		'user':user,
+		'posts':postsrep
+	}
+	return render(request,'hello.html',context)
